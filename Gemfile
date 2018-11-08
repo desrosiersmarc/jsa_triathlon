8b45819840e648d8d8a2cd3a229032672f30db96
marc desrosiers <mdesrosiers@norauto.fr>
2018-11-08 22:48:31 +0100

    Change pictures

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem 'webpacker'
 
 # Personal gem
 gem 'cloudinary'
-
+gem 'carrierwave', '~> 1.2'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,33 @@
 class PicturesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-
+#TODO remove :new
   def index
     @pictures = Picture.all
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to pictures_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  def picture_params
+    params.require(:picture).permit(
+      :link,
+      :title,
+      :user_id,
+      :date,
+      :like,
+      :photo)
+  end
+
 end
