3fabd1c8bee1cab1c98e9188bfa96d67abdb063e
marc desrosiers <mdesrosiers@norauto.fr>
2018-12-12 00:00:11 +0100

    create article and article_type. Update seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181113140706) do
+ActiveRecord::Schema.define(version: 20181211224051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date"
+    t.time "start_time"
+    t.time "end_time"
+    t.string "location"
+    t.string "link"
+    t.string "content"
+    t.bigint "user_id"
+    t.bigint "article_type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_type_id"], name: "index_articles_on_article_type_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string "title"
@@ -44,5 +66,7 @@ ActiveRecord::Schema.define(version: 20181113140706) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "article_types"
+  add_foreign_key "articles", "users"
   add_foreign_key "pictures", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Delete pictures"
+puts "Delete all"
   Picture.delete_all
+  Article.delete_all
+  ArticleType.delete_all
+  User.delete_all
+
+puts "Create user"
+user_created = User.new(email: "md@md.fr", password: "123soleil", firstname: "Marc", lastname: "Desrosiers", birthday: '01/11/1972', mobil_phone: '0662262352')
+user_created.save
+
 
 puts "Create pictures"
 url="http://eu.ironman.com/~/media/e52189b7e5f94db0bb2d43e717ec87ad/28092017%20rotator%20im703nice%203.jpg?w=1600&h=980&c=1"
@@ -26,4 +34,28 @@ url="http://eu.ironman.com/~/media/e52189b7e5f94db0bb2d43e717ec87ad/28092017%20r
   like: 5)
   pict.remote_photo_url = url
   pict.save
+
+puts "Create Article Types"
+  ArticleType.create(name: 'club')
+  ArticleType.create(name: 'training')
+  ArticleType.create(name: 'contest')
+  ArticleType.create(name: 'school')
+  ArticleType.create(name: 'partner')
+puts "Article Types created"
+
+puts "Create Articles"
+  20.times do Article.create(
+    name: Faker::FunnyName.name,
+    date: Time.now.strftime('%v'),
+    start_time: Time.now.strftime('%T'),
+    end_time: Time.now.strftime('%T'),
+    location: Faker::Address.city,
+    link: Faker::Internet.url,
+    content: Faker::Lorem.paragraph(rand(0..3)),
+    user: User.all.sample,
+    article_type: ArticleType.all.sample)
+    print "*"
+  end
+puts ""
+puts "Articles created"
 
