3fbf4376c776dfc3d65817d1e5b9abceb8c37888
marc desrosiers <mdesrosiers@norauto.fr>
2018-12-13 15:50:41 +0100

    create article : model, controler, view and add validates, refacto the seed, and work on date and time design

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :article_type
+
+  validates :name, presence: true, length: {maximum: 30, minimum: 10}
+  validates :date, presence: true
+  validates :article_type_id, presence: true
 end
