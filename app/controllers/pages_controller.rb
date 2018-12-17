0fb41f0a696f24df9609aa295a33fb73fefefc71
marc desrosiers <mdesrosiers@norauto.fr>
2018-12-17 20:23:12 +0100

    Create controller to display informations on right wrapper and correct an issue to display width of card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,24 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :calendar]
 
   def home
-    @picture = Picture.last
+    @pictures = Picture.all
+                      .sort_by { |article| article.updated_at}
+                      .reverse
+                      .take(3)
+    @club_events = select_articles(1)
+    @training = Article.where(article_type: 2).last
+    @contests = select_articles(2)
+    @schools = select_articles(3)
+    @results = select_articles(6)
+    #TODO @picture for mobile and @pictures > mobile
+
+  end
+
+private
+  def select_articles(article_type)
+    Article.where(article_type: article_type)
+            .sort_by { |article| article.updated_at}
+            .reverse
+            .take(3)
   end
 end
