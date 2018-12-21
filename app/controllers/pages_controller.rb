07b4f1c4de34311e19584300448671124b9b0ffa
marc desrosiers <mdesrosiers@norauto.fr>
2018-12-21 10:11:42 +0100

    Update page controller and create pages for article selections, improve link solution and create div to display in function of user role

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,52 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :calendar]
+  before_action :select_contests, only: [:home, :contests]
+  before_action :select_club_events, only: [:home, :club_events]
+  before_action :select_school, only: [:home, :school]
+  before_action :select_results, only: [:home, :results]
 
   def home
     @pictures = Picture.all
                       .sort_by { |article| article.updated_at}
                       .reverse
                       .take(3)
-    @club_events = select_articles(1)
-    @contests = select_articles(3)
-    @schools = select_articles(4)
-    @results = select_articles(6)
+
+    # 2 Training
+    # 5 Partner
+
+    @club_events_top3 = @club_events.take(3)
+    @contests_top3 = @contests.take(3)
+    @schools_top3 = @schools.take(3)
+    @results_top3 = @results.take(3)
     # 2 Training
     # 5 Partner
 
 
+
   end
+
 
 private
   def select_articles(article_type)
     Article.where(article_type: article_type)
             .sort_by { |article| article.updated_at}
             .reverse
-            .take(3)
   end
+
+  def select_contests
+    @contests = select_articles(3)
+  end
+
+  def select_club_events
+    @club_events = select_articles(1)
+  end
+
+  def select_school
+    @schools = select_articles(4)
+  end
+
+  def select_results
+    @results = select_articles(6)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'calendar', to: 'pages#calendar'
-  resources :pictures, only: [:index, :new, :create, :show]
+  get 'club_events', to: 'pages#club_events'
+  get 'trainings', to: 'pages#trainings'
+  get 'contests', to: 'pages#contests'
+  get 'school', to: 'pages#school'
+  get 'results', to: 'pages#results'
+
+
+
+  resources :pictures, only: [:index, :new, :create, :show, :edit, :update]
+  #TODO create update method
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
