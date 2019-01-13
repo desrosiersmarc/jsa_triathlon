87282ff32de1851ad8cff3e5823075bc9490ab43
marc desrosiers <mdesrosiers@norauto.fr>
2019-01-13 18:59:25 +0100

    finish the article selection for impots et add a gradient animation

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -2,13 +2,15 @@ class ClubsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index ]
 
   def index
-    @clubs = Article.where(article_type: 7)
+    @clubs = Article.where(article_type: 7) + Article.where(article_type: 8)
+
   end
 
   def finances
   end
 
   def impots
+    @impots = Article.where(article_type: 8).first
   end
 
 end
