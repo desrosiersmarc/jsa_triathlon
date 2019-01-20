4f9bb574bf9c37526718eef1a2ddcaaee2e8a8a5
marc desrosiers <mdesrosiers@norauto.fr>
2019-01-20 17:40:40 +0100

    hide js to test compilation

--- app/assets/javascripts/close-div.js
+++ app/assets/javascripts/close-div.js
@@ -1,19 +1,19 @@
-let closeTaxe = document.getElementById("taxes");
-let closeTaxeShift = document.getElementById("taxes-shift");
-let closeTaxeButton = document.getElementById("taxes-close");
+// let closeTaxe = document.getElementById("taxes");
+// let closeTaxeShift = document.getElementById("taxes-shift");
+// let closeTaxeButton = document.getElementById("taxes-close");
 
-closeTaxeButton.onclick = function() {
-  closeTaxeDiv();
-};
+// closeTaxeButton.onclick = function() {
+//   closeTaxeDiv();
+// };
 
-function closeTaxeDiv(){
-  closeTaxe.style.display = "none";
-  closeTaxeShift.style.display = "none";
-  sessionStorage.setItem('closeTaxeDisplay', 'false');
-};
+// function closeTaxeDiv(){
+//   closeTaxe.style.display = "none";
+//   closeTaxeShift.style.display = "none";
+//   sessionStorage.setItem('closeTaxeDisplay', 'false');
+// };
 
-var closeTaxeDisplay = sessionStorage.getItem('closeTaxeDisplay');
+// var closeTaxeDisplay = sessionStorage.getItem('closeTaxeDisplay');
 
-if (closeTaxeDisplay == 'false') {
-  closeTaxeDiv();
-};
+// if (closeTaxeDisplay == 'false') {
+//   closeTaxeDiv();
+// };
