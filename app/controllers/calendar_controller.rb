6eab05f819e4036274a5b950e6abe79898102f19
marc desrosiers <mdesrosiers@norauto.fr>
2019-02-04 16:54:51 +0100

    create google calendar solutions

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -5,7 +5,58 @@ class CalendarController < ApplicationController
     redirect_to client.authorization_uri.to_s
   end
 
-  private
+  def callback
+    client = Signet::OAuth2::Client.new(client_options)
+    client.code = params[:code]
+
+    response = client.fetch_access_token!
+
+    session[:authorization] = response
+
+    redirect_to calendars_url
+  end
+
+  def calendars
+    client = Signet::OAuth2::Client.new(client_options)
+    client.update!(session[:authorization])
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
+
+    @calendar_list = service.list_calendar_lists
+    rescue Google::Apis::AuthorizationError
+    response = client.refresh!
+
+    session[:authorization] = session[:authorization].merge(response)
+
+    retry
+  end
+
+  def events
+    client = Signet::OAuth2::Client.new(client_options)
+    client.update!(session[:authorization])
+
+    service = Google::Apis::CalendarV3::CalendarService.new
+    service.authorization = client
+    @event_list = service.list_events('angoulemetriathlon@gmail.com',
+    # @event_list = service.list_events('2vcjtroc34lnia389foc9kv06g@group.calendar.google.com',
+                                      max_results: 200,
+                                      show_deleted: false,
+                                      #max_results: 3,
+                                      single_events: true,
+                                      order_by: 'startTime',
+                                      time_min: DateTime.now.rfc3339,
+                                      time_max: (DateTime.now + 7.day).rfc3339)
+    # @event_list = service.list_events('nola0kh57fg6hikuehsifepno4@group.calendar.google.com')
+    rescue Google::Apis::AuthorizationError
+    response = client.refresh!
+
+    session[:authorization] = session[:authorization].merge(response)
+
+    retry
+  end
+
+private
 
   def client_options
     {
