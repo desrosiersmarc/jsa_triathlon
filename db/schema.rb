750448316f95e2865e20be059932e59333578fa1
marc desrosiers <mdesrosiers@norauto.fr>
2019-02-12 22:06:18 +0100

    rails db:migrate and add display_profil to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190207183541) do
+ActiveRecord::Schema.define(version: 20190212210004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20190207183541) do
     t.string "profil"
     t.boolean "office_member"
     t.boolean "coach"
+    t.boolean "display_profil"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
