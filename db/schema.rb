2212356cf2d7c5c71cc035c510e6503b3f3e7ecd
marc desrosiers <mdesrosiers@norauto.fr>
2019-04-24 15:00:18 +0200

    Create a new email version and add a solution to send an email to a list of people

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190416194806) do
+ActiveRecord::Schema.define(version: 20190424113645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 20190416194806) do
     t.boolean "coach"
     t.boolean "display_profil", default: true
     t.boolean "member"
+    t.boolean "notification", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
