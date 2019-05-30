52bd145f9bddd5ac7eaa5b3b7b880a9db5f70421
marc desrosiers <mdesrosiers@norauto.fr>
2019-05-30 23:22:41 +0200

    create a group mailing column for user and update controller. RAILS DB:MIGRATE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190529145223) do
+ActiveRecord::Schema.define(version: 20190530210452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -168,6 +168,7 @@ ActiveRecord::Schema.define(version: 20190529145223) do
     t.boolean "display_profil", default: true
     t.boolean "member"
     t.boolean "notification", default: true
+    t.integer "mailing_group", default: 2
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
