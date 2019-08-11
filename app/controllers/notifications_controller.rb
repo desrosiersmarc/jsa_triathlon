2db71fd7c2028ed8a628de6fdeba8bd16a192cc4
marc desrosiers <mdesrosiers@norauto.fr>
2019-08-12 00:33:08 +0200

    create form to update read to true to delete the notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,36 @@
 class NotificationsController < ApplicationController
+  before_action :find_notification, only: [:edit, :update]
 
   def index
-    @my_notifications = Notification.where(user_id: current_user.id)
+    @my_notifications = Notification.where(user_id: current_user.id).reverse
   end
+
+  def edit
+
+  end
+
+  def update
+    @notification.update(notification_params)
+    if @notification.save
+      redirect_to notifications_path
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def find_notification
+    @notification = Notification.find(params[:id])
+  end
+
+  def notification_params
+    params.require(:notification).permit(
+      :article_id,
+      :picture_id,
+      :product_id,
+      :read,
+      :user_id)
+  end
+
 end
