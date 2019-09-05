30611e215c9aabe54c753d3ab064e0bc1f84def5
marc desrosiers <mdesrosiers@norauto.fr>
2019-09-05 12:01:25 +0200

    create a div closeable to subscription informations

---
+++ app/javascript/close-div-hp.js
@@ -0,0 +1,6 @@
+let divToClose = document.getElementById("hp-new-content");
+let closeDivButton = document.getElementById("close-div-button");
+
+closeDivButton.onclick = function () {
+  divToClose.style.display = "none";
+};
