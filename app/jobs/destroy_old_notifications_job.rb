3af58a727bdbda34237ce5f0ce9b980e576940db
marc desrosiers <mdesrosiers@norauto.fr>
2019-09-17 20:46:55 +0200

    add created_at condition and add number of days

--- app/jobs/destroy_old_notifications_job.rb
+++ app/jobs/destroy_old_notifications_job.rb
@@ -2,6 +2,7 @@ class DestroyOldNotificationsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Notification.where("updated_at < ?", Time.now-0.day).destroy_all
+    Notification.where("updated_at < ?", Time.now-15.day).destroy_all
+    Notification.where("created_at < ?", Time.now-15.day).destroy_all
   end
 end
