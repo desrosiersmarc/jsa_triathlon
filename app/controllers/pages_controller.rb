54d36561b7029883d6a980c629b7f84002711044
marc desrosiers <mdesrosiers@norauto.fr>
2020-05-16 08:49:19 +0200

    try to replace take by first

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,12 +16,17 @@ class PagesController < ApplicationController
                       .take(3)
     # 5 Partner
     @club = Club.all.first
-    @clubs = @clubs.take(3)
+    # @clubs = @clubs.take(3)
+    @clubs = @clubs.first
     @club_events_top3 = select_articles_homepage(1)
     @contests_top3 = select_articles_homepage(3)
     @schools_top3 = select_articles_homepage(4)
-    @results_top3 = @results.take(3)
+    # @results_top3 = @results.take(3)
+    @results_top3 = @results.first
     @trainings_top3 = select_articles_homepage(2)
+
+
+
 
     @product_echange_count = Product.where(product_type_id: 2, active: true).count
 
