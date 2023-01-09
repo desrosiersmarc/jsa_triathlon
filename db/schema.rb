1465dbc0fe417b82ec754decdf404dc9b8adecc6
Marc DESROSIERS <mdesrosiers.pro@gmail.com>
2023-01-09 22:44:56 +0100

    Revert "Test and solve issue with new rails and ruby version"

--- db/schema.rb
+++ db/schema.rb
@@ -2,46 +2,18 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_06_171037) do
+ActiveRecord::Schema.define(version: 20210929192349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
 
   create_table "article_types", force: :cascade do |t|
     t.string "name"
@@ -65,9 +37,7 @@ ActiveRecord::Schema.define(version: 2023_01_06_171037) do
     t.boolean "active", default: true
     t.boolean "send_email", default: false
     t.boolean "send_email_admin", default: true
-    t.bigint "time_period_id"
     t.index ["article_type_id"], name: "index_articles_on_article_type_id"
-    t.index ["time_period_id"], name: "index_articles_on_time_period_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
@@ -245,14 +215,6 @@ ActiveRecord::Schema.define(version: 2023_01_06_171037) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "time_periods", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_time_periods_on_ancestry"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -292,10 +254,7 @@ ActiveRecord::Schema.define(version: 2023_01_06_171037) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "article_types"
-  add_foreign_key "articles", "time_periods"
   add_foreign_key "articles", "users"
   add_foreign_key "authors", "articles"
   add_foreign_key "authors", "users"
